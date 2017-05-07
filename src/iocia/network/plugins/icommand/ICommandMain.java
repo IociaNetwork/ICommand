88c37d93f155943a9d46be74938aa8d7486e76e0
IociaNetwork <iocianetwork@gmail.com>
2017-05-06 21:11:48 -0600

    Added ICommandMain

---
+++ src/iocia/network/plugins/icommand/ICommandMain.java
@@ -0,0 +1,17 @@
+package iocia.network.plugins.icommand;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Only here so the plugin may be initialized like
+ * a regular plugin.
+ */
+public class ICommandMain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {}
+
+    @Override
+    public void onDisable() {}
+
+}
